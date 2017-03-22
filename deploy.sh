f0b362d0ee786b6856793dd3ae65d666b76fb312
盧瑞山 <admin@luruishande-MacBook-Pro.local>
2017-03-22 18:16:48 +0800

    fix deploy.js

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,6 @@ set -e
 
 sudo /opt/google-cloud-sdk/bin/gcloud docker -- push us.gcr.io/${PROJECT_NAME}/hello
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
+sudo /opt/google-cloud-sdk/bin/gcloud container clusters get-credentials  docker-hello-google-cluster \
+--zone us-central1-f --project circle-ctl-test-162119
 kubectl patch deployment docker-hello-google -p '{"spec":{"template":{"spec":{"containers":[{"name":"docker-hello-google-cluster","image":"us.gcr.io/circle-ctl-test/hello:'"$CIRCLE_SHA1"'"}]}}}}'
