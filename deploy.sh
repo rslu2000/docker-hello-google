31e4a020eef02c57f911b8177d1efb73e8f29eeb
盧瑞山 <admin@luruishande-MacBook-Pro.local>
2017-03-25 18:55:48 +0800

    fixed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,5 @@ sudo /opt/google-cloud-sdk/bin/gcloud docker -- push us.gcr.io/${PROJECT_NAME}/h
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
 sudo /opt/google-cloud-sdk/bin/gcloud config set account \
 circle2google-sa@circle-ctl-test-162119.iam.gserviceaccount.com
-sudo /opt/google-cloud-sdk/bin/gcloud container clusters get-credentials  docker-hello-google-cluster \
---zone us-central1-f --project circle-ctl-test-162119
 kubectl config set-credentials cluster-admin --client-key=/home/ubuntu/account-auth.json
-kubectl patch deployment gke-docker-hello-google--default-pool-1434b3f4-nmz2 -p '{"spec":{"template":{"spec":{"containers":[{"name":"docker-hello-google-cluster","image":"us.gcr.io/circle-ctl-test-162119/hello:'"$CIRCLE_SHA1"'"}]}}}}'
+kubectl patch deployment gke-docker-hello -p '{"spec":{"template":{"spec":{"containers":[{"name":"docker-hello-google-cluster","image":"us.gcr.io/circle-ctl-test-162119/hello:'"$CIRCLE_SHA1"'"}]}}}}'
