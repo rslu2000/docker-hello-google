db7b167a3b441221b286016e2e2455f4b03b0533
盧瑞山 <admin@luruishande-MacBook-Pro.local>
2017-03-25 17:46:14 +0800

    fix the deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,5 @@ sudo /opt/google-cloud-sdk/bin/gcloud config set account \
 circle2google-sa@circle-ctl-test-162119.iam.gserviceaccount.com
 sudo /opt/google-cloud-sdk/bin/gcloud container clusters get-credentials  docker-hello-google-cluster \
 --zone us-central1-f --project circle-ctl-test-162119
+kubectl config set-credentials cluster-admin --client-key=/home/ubuntu/account-auth.json
 kubectl patch deployment docker-hello-google -p '{"spec":{"template":{"spec":{"containers":[{"name":"docker-hello-google","image":"us.gcr.io/circle-ctl-test/hello:'"$CIRCLE_SHA1"'"}]}}}}'
