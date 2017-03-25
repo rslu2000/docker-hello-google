fac355a50216f83f7531f01b53192a85b787c0f3
盧瑞山 <admin@luruishande-MacBook-Pro.local>
2017-03-25 10:28:45 +0800

    fix the deply.sh

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,8 @@ set -e
 
 sudo /opt/google-cloud-sdk/bin/gcloud docker -- push us.gcr.io/${PROJECT_NAME}/hello
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
+sudo /opt/google-cloud-sdk/bin/gcloud config set account \
+circle2google-sa@circle-ctl-test-162119.iam.gserviceaccount.com
 sudo /opt/google-cloud-sdk/bin/gcloud container clusters get-credentials  docker-hello-google-cluster \
 --zone us-central1-f --project circle-ctl-test-162119
 kubectl patch deployment docker-hello-google -p '{"spec":{"template":{"spec":{"containers":[{"name":"docker-hello-google","image":"us.gcr.io/circle-ctl-test/hello:'"$CIRCLE_SHA1"'"}]}}}}'
