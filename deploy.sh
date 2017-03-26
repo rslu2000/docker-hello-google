ae5672c2bec23aabcc40dd0223208861fcca817c
盧瑞山 <admin@luruishande-MacBook-Pro.local>
2017-03-26 20:54:43 +0800

    fixed the deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,8 @@ sudo /opt/google-cloud-sdk/bin/gcloud container clusters get-credentials  docker
 --zone us-central1-f --project circle-ctl-test-162119
 kubectl config set-credentials cluster-admin --client-key=/home/ubuntu/account-auth.json
 # kubectl run docker-hello-google --image=us.gcr.io/${PROJECT_NAME}/hello:$CIRCLE_SHA1
-kubectl patch deployment docker-hello-google -p '{"spec":{"template":{"spec":{"containers":[{"name":"docker-hello-google-cluster","image":"us.gcr.io/circle-ctl-test-162119/hello:'"$CIRCLE_SHA1"'"}]}}}}'
+#kubectl patch deployment docker-hello-google -p '{"spec":{"template":{"spec":{"containers":[{"name":"docker-hello-google-cluster","image":"us.gcr.io/circle-ctl-test-162119/hello:'"$CIRCLE_SHA1"'"}]}}}}'
+kubectl patch deployment docker-hello-google -p '{"spec":{"template":{"spec":{"containers":[{"name":"docker-hello-google-cluster","image":"us.gcr.io/circle-ctl-test-162119/hello"}]}}}}'
 kubectl expose deployment docker-hello-google --type="LoadBalancer" --port=80 --target-port=3000
 kubectl get services docker-hello-google
 kubectl describe services docker-hello-google
